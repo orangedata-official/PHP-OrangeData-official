7ce41adb0dd3f54a5b14baa1829a322ae9189dc2
Patyrsa <patyrsa@gmail.com>
2017-06-22 22:40:03 +0300

    simple example using client class

---
+++ example.php
@@ -0,0 +1,30 @@
+
+<?php
+
+include_once 'orangedata_client.php';
+$api_url='https://apip.orangedata.ru:2443/api/v2/documents/';
+
+$sign_pkey = getcwd().'/secure_path/private_key.pem';
+
+//create new client
+$byer = new orangedata\orangedata_client('0123456789', //ИНН
+        $api_url,
+        $sign_pkey,//private key for signing
+        getcwd() . '/secure_path/client_ca.crt', //path to CA CERTIFICATE
+        getcwd() . '/secure_path/client.pem',//path to Client cert in PEM
+        'Str0ngP@$$w0rD'); //password for client cert 
+
+// create client new order, add positions , add payment, send request
+$result = $byer
+        ->is_debug()
+        ->create_order(2, 1, 'a@b',1)
+        ->add_position_to_order(6,10,1,'mathes')
+        ->add_payment_to_order(1, 10)
+        ->add_payment_to_order(2, 50)
+        ->send_order();
+//view response
+var_dump($result);
+
+//view status of order 
+var_dump($byer->get_order_status(2));
+?>
