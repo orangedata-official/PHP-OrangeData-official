81a34bdfbcd1717adbc538631bd92cb2d1de0a55
Patyrsa <patyrsa@gmail.com>
2017-07-06 15:28:59 +0400

    friendlify (#4)

--- example.php
+++ example.php
@@ -2,23 +2,30 @@
 <?php
 
 include_once 'orangedata_client.php';
+
 $api_url='https://apip.orangedata.ru:2443/api/v2/documents/';
-
-$sign_pkey = getcwd().'/secure_path/private_key.pem';
-
+$sign_pkey = getcwd().'/secure_path/private_key.pem';//private key for signing
+$ssl_client_key = getcwd().'/secure_path/client.key';//path to client private key for ssl
+$ssl_client_crt = getcwd().'/secure_path/client.crt';//path to client certificate for ssl
+$ssl_client_crt_pass = 'password';//password for client certificate for ssl
+$inn = '0123456789';//ИНН
+//
 //create new client
-$byer = new orangedata\orangedata_client('0123456789', //ИНН
+$byer = new orangedata\orangedata_client($inn, 
         $api_url,
-        $sign_pkey,//private key for signing
-        getcwd() . '/secure_path/client.pem',//path to Client cert in PEM
-        'Str0ngP@$$w0rD'); //password for client cert 
+        $sign_pkey,
+        $ssl_client_key,
+        $ssl_client_crt,
+        $ssl_client_crt_pass); //password for client cert 
+
+//for sandbox
+$byer->is_debug()->set_ca_cert(getcwd() . '/secure_path/server.crt'); //path to ca certificate for suport selfsigned certificates
+   
 
 // create client new order, add positions , add payment, send request
 $result = $byer
-        ->is_debug()
-        ->set_ca_cert(getcwd() . '/secure_path/client_ca.crt')//path to CA CERTIFICATE
         ->create_order(2, 1, 'a@b',1)
-        ->add_position_to_order(6,10,1,'mathes')
+        ->add_position_to_order(6,10,1,'matches')
         ->add_payment_to_order(1, 10)
         ->add_payment_to_order(2, 50)
         ->send_order();
