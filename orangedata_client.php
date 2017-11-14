3f863e81bc3c89c10cff4c908fc8487b66017904
silent105 <kovalunas@yandex.ru>
2017-11-14 14:55:16 +0300

    Revert "Library v2.1.0"
    This reverts commit 7c919da4cfa14a38fd2b3dabff775920ee5f8e02.

--- orangedata_client.php
+++ orangedata_client.php
@@ -2,8 +2,8 @@
 
 /**
  * OrangeDataClient PHP
- * Version of PHP: 5.6.6+
- * @version 2.1.0
+ * Version of PHP: 5.4+
+ * @version 2.0.1
  */
 
 namespace orangedata;
@@ -14,8 +14,7 @@ use \Exception;
 class orangedata_client {
 
     private $order_request;
-    private $correction_request;
-    private $api_url;
+    private $url;
     private $inn;
     private $debug_file;
     private $debug = false;
@@ -27,16 +26,16 @@ class orangedata_client {
 
     /**
      * @param mixed $inn
-     * @param mixed $api_url
+     * @param mixed $url
      * @param string $sign_pkey - Path to signing private key or his PEM body
      * @param string $client_key - Path to client private key
      * @param string $client_cert - Path to Client 2SSL Certificate
      * @param string $ca_cert - Path to CA Certificate
      * @param string $client_cert_pass - Password for Client 2SSL Certificate
      */
-    public function __construct($inn, $api_url, $sign_pkey, $client_key, $client_cert, $ca_cert, $client_cert_pass) {
+    public function __construct($inn, $url, $sign_pkey, $client_key, $client_cert, $ca_cert, $client_cert_pass) {
         $this->inn = (string) $inn;
-        $this->api_url = $api_url;
+        $this->url = (string) $url;
         $this->private_key_pem = (string) $sign_pkey;
         $this->client_pkey = (string) $client_key;
         $this->client_cert = (string) $client_cert;
@@ -66,15 +65,17 @@ class orangedata_client {
      *  @throws Exception
      */
     public function create_order($id, $type, $customerContact, $taxationSystem, $group = null) {
+        
         $this->order_request = new \stdClass();
         $this->order_request->id = (string) $id;
         $this->order_request->inn = $this->inn;
         $this->order_request->group = $group ?: 'Main';
+
         $this->order_request->content = new \stdClass();
         if (is_int($type) and preg_match('/^[1234]$/', $type)) {
             $this->order_request->content->type = $type;
         } else {
-            throw new Exception('Incorrect order Type' . PHP_EOL);
+            throw new Exception('Incorrect order Type');
         }
         $this->order_request->content->positions = array();
         $this->order_request->content->checkClose = new \stdClass();
@@ -82,12 +83,12 @@ class orangedata_client {
         if (preg_match('/^[012345]$/', $taxationSystem)) {
             $this->order_request->content->checkClose->taxationSystem = $taxationSystem;
         } else {
-            throw new Exception('Incorrect taxationSystem' . PHP_EOL);
+            throw new Exception('Incorrect taxationSystem');
         }
         if (filter_var($customerContact, FILTER_VALIDATE_EMAIL) or preg_match('/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/', $customerContact)) {
             $this->order_request->content->customerContact = $customerContact;
         } else {
-            throw new Exception('Incorrect customer Contact' . PHP_EOL);
+            throw new Exception('Incorrect customer Contact');
         }
         return $this;
     }
@@ -138,13 +139,13 @@ class orangedata_client {
             $position->text = $text;
             $this->order_request->content->positions[] = $position;
         } else {
-            throw new Exception('Invalid Position Quantity, Price, Tax or Text' . PHP_EOL);
+            throw new Exception('Invalid Position Quantity, Price, Tax or Text');
         }
         if ((preg_match('/^[1-7]$/', $paymentMethodType) or is_null($paymentMethodType)) and ( preg_match('/^[1-9]{1}$|^1[0-3]{1}$/', $paymentSubjectType) or is_null($paymentSubjectType))) {
             $position->paymentMethodType = $paymentMethodType ?: 4;
             $position->paymentSubjectType = $paymentSubjectType ?: 1;
         } else {
-            throw new Exception('Invalid Position paymentMethodType or paymentSubjectType' . PHP_EOL);
+            throw new Exception('Invalid Position paymentMethodType or paymentSubjectType');
         }
         return $this;
     }
@@ -168,95 +169,7 @@ class orangedata_client {
             $payment->amount = (float) $amount;
             $this->order_request->content->checkClose->payments[] = $payment;
         } else {
-            throw new Exception('Invalid Payment Type or Amount' . PHP_EOL);
-        }
-        return $this;
-    }
-
-    /**
-     * add_agent_to_order(a, b, c, d, e, f, g, h, i) - Добавление агента
-     *  @param $agentType (a) - Признак агента, 1057. Битовое поле, где номер бита обозначает, что оказывающий услугу покупателю (клиенту) пользователь является (Число от 1 до 127):
-     *      0 – банковский платежный агент
-     *      1 – банковский платежный субагент
-     *      2 – платежный агент
-     *      3 – платежный субагент
-     *      4 – поверенный
-     *      5 – комиссионер
-     *      6 – иной агент
-     *  @param $payTOP (b) - Телефон оператора перевода, 1075 (Массив строк длиной от 1 до 19 символов, формат +{Ц})
-     *  @param $payAO (c) - Операция платежного агента, 1044 (Строка длиной от 1 до 24 символов)
-     *  @param $payAPN (d) - Телефон платежного агента, 1073 (Массив строк длиной от 1 до 19 символов, формат +{Ц})
-     *  @param $payOPN (e) - Телефон оператора по приему платежей, 1074 (Массив строк длиной от 1 до 19 символов, формат +{Ц})
-     *  @param $payON (f) - Наименование оператора перевода, 1026 (Строка длиной от 1 до 64 символов)
-     *  @param $payOA (g) - Адрес оператора перевода, 1005 (Строка длиной от 1 до 244 символов)
-     *  @param $payOpINN (h) - ИНН оператора перевода, 1016 (Строка длиной от 10 до 12 символов, формат ЦЦЦЦЦЦЦЦЦЦ)
-     *  @param $supPN (i) - Телефон поставщика, 1171 (Массив строк длиной от 1 до 19 символов, формат +{Ц})
-     *  @return $this
-     *  @throws Exception
-     */
-    public function add_agent_to_order($agentType, $payTOP, $payAO, $payAPN, $payOPN, $payON, $payOA, $payOpINN, $supPN) {
-        if ($agentType > 0 and $agentType < 128) {
-            $this->order_request->content->agentType = $agentType;
-        } else {
-            throw new Exception('Invalid agentType' . PHP_EOL);
-        }
-        for ($i = 0; $i < count($payTOP); $i++) {
-            if (!preg_match('/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/', $payTOP[$i]))
-                throw new Exception('Invalid paymentTransferOperatorPhoneNumbers' . PHP_EOL);
-            if ($i == count($payTOP) - 1) {
-                $this->order_request->content->paymentTransferOperatorPhoneNumbers = $payTOP;
-            }
-        }
-        if (strlen($payAO) > 0 and strlen($payAO) < 25) {
-            $this->order_request->content->paymentAgentOperation = $payAO;
-        } else {
-            throw new Exception('Invalid paymentAgentOperation' . PHP_EOL);
-        }
-        for ($i = 0; $i < count($payAPN); $i++) {
-            if (!preg_match('/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/', $payAPN[$i]))
-                throw new Exception('Invalid paymentAgentPhoneNumbers' . PHP_EOL);
-            if ($i == count($payAPN) - 1) {
-                $this->order_request->content->paymentAgentPhoneNumbers = $payAPN;
-            }
-        }
-        for ($i = 0; $i < count($payOPN); $i++) {
-            if (!preg_match('/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/', $payOPN[$i]))
-                throw new Exception('Invalid paymentOperatorPhoneNumbers' . PHP_EOL);
-            if ($i == count($payOPN) - 1) {
-                $this->order_request->content->paymentOperatorPhoneNumbers = $payOPN;
-            }
-        }
-        if (strlen($payON) > 0 && strlen($payON) < 65 and strlen($payOA) && strlen($payOA) < 245 and strlen($payOpINN) > 9 && strlen($payOpINN) != 11 && strlen($payOpINN) < 13) {
-            $this->order_request->content->paymentOperatorName = $payON;
-            $this->order_request->content->paymentOperatorAddress = $payOA;
-            $this->order_request->content->paymentOperatorINN = $payOpINN;
-        } else {
-            throw new Exception('Invalid paymentOperatorName, paymentOperatorAddress or paymentOperatorINN' . PHP_EOL);
-        }
-        for ($i = 0; $i < count($supPN); $i++) {
-            if (!preg_match('/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/', $supPN[$i]))
-                throw new Exception('Invalid supplierPhoneNumbers' . PHP_EOL);
-            if ($i == count($supPN) - 1) {
-                $this->order_request->content->supplierPhoneNumbers = $supPN;
-            }
-        }
-        return $this;
-    }
-
-    /**
-     * add_user_attribute(a, b) - Добавление дополнительного реквизита пользователя, 1084
-     *  @param $name (a) - Наименование дополнительного реквизита пользователя, 1085 (Строка от 1 до 64 символов)
-     *  @param $value (b) - Значение дополнительного реквизита пользователя, 1086 (Строка от 1 до 175 символов)
-     *  @return $this
-     *  @throws Exception
-     */
-    public function add_user_attribute($name, $value) {
-        if (strlen($name) > 0 && strlen($name) < 65 and strlen($value) > 0 && strlen($value) < 176) {
-            $this->order_request->content->additionalUserAttribute = new \stdClass();
-            $this->order_request->content->additionalUserAttribute->name = $name;
-            $this->order_request->content->additionalUserAttribute->value = $value;
-        } else {
-            throw new Exception('Sting Name or Value is too long' . PHP_EOL);
+            throw new Exception('Invalid Payment Type or Amount');
         }
         return $this;
     }
@@ -269,7 +182,7 @@ class orangedata_client {
     public function send_order() {
         $jsonstring = json_encode($this->order_request, JSON_PRESERVE_ZERO_FRACTION);
         $sign = $this->sign_order_request($jsonstring);
-        is_numeric($this->api_url) ? $curl =  $this->prepare_curl($this->edit_url($this->api_url, TRUE)) : $curl =  $this->prepare_curl($this->api_url . '/api/v2/documents/');
+        $curl = $this->prepare_curl($this->url . '/api/v2/documents/');
         $headers = array(
             "Content-Length: " . strlen($jsonstring),
             "Content-Type: application/json; charset=utf-8",
@@ -318,7 +231,7 @@ class orangedata_client {
         if (strlen($id) > 32 OR strlen($id) == 0) {
             throw new Exception('Invalid order identifier');
         }
-        $curl = is_int($this->api_url) ? $this->prepare_curl($this->edit_url($this->api_url, TRUE) . $this->inn . '/status/' . $id) : $this->prepare_curl($this->api_url . '/api/v2/documents/' . $this->inn . '/status/' . $id);
+        $curl = $this->prepare_curl($this->url . '/api/v2/documents/' . $this->inn . '/status/' . $id);
         curl_setopt($curl, CURLOPT_POST, false);
         $answer = curl_exec($curl);
         $info = curl_getinfo($curl);
@@ -353,24 +266,6 @@ class orangedata_client {
         } else {
             return false;
         }
-    }
-
-    /**
-     * edit_url(a, b) - автоматическая генерация ссылки
-     *  @param $port (a) - номер порта, на который шлются запросы
-     *  @param $tag (b) - идентификатор отправки/проверки чека
-     *  @return string
-     *  @throws Exception
-     */
-    private function edit_url($port, $tag) {
-        if ($port == 2443 or $port == 12001){
-            $tag ? $url = 'https://apip.orangedata.ru:' . $port . '/api/v2/documents/' : $url = 'https://apip.orangedata.ru:' . $port . '/api/v2/corrections/';
-        } elseif ($port == 12003) {
-            $tag ? $url = 'https://api.orangedata.ru:' . $port . '/api/v2/documents/' : $url = 'https://api.orangedata.ru:' . $port . '/api/v2/corrections/';
-        } else {
-            throw new Exception('Port error: invalid value' . PHP_EOL);
-        }
-        return $url;
     }
 
     private function prepare_curl($url) {
@@ -440,16 +335,18 @@ class orangedata_client {
         $this->correction_request->inn = $this->inn;
         $this->correction_request->group = $group ?: 'Main';
         $this->correction_request->key = $key;
+
         $this->correction_request->content = new \stdClass();
+
         if (is_numeric($correctionType) and preg_match('/^[01]$/', $correctionType)) {
             $this->correction_request->content->correctionType = (int) $correctionType;
         } else {
-            throw new Exception('Incorrect correction CorrectionType' . PHP_EOL);
+            throw new Exception('Incorrect correction CorrectionType');
         }
         if (is_numeric($type) and preg_match('/^[13]$/', $type)) {
             $this->correction_request->content->type = (int) $type;
         } else {
-            throw new Exception('Incorrect correction Type' . PHP_EOL);
+            throw new Exception('Incorrect correction Type');
         }
         $this->correction_request->content->description = substr((string) $description, 1, 244);
         $this->correction_request->content->causeDocumentDate = $causeDocumentDate->setTime(0, 0)->format(DateTime::ISO8601);
@@ -466,10 +363,11 @@ class orangedata_client {
         $this->correction_request->content->tax4Sum = (float) $tax4Sum;
         $this->correction_request->content->tax5Sum = (float) $tax5Sum;
         $this->correction_request->content->tax6Sum = (float) $tax6Sum;
+
         if (preg_match('/^[012345]$/', $taxationSystem)) {
             $this->correction_request->content->taxationSystem = $taxationSystem;
         } else {
-            throw new Exception('Incorrect taxationSystem' . PHP_EOL);
+            throw new Exception('Incorrect taxationSystem');
         }
         return $this;
     }
@@ -485,7 +383,7 @@ class orangedata_client {
             throw  new Exception('JSON encode error:' . json_last_error_msg());
         }
         $sign = $this->sign_order_request($jsonstring);
-        $curl = is_numeric($this->api_url) ? $this->prepare_curl($this->edit_url($this->api_url, FALSE)) : $this->prepare_curl($this->api_url . '/api/v2/corrections/');
+        $curl = $this->prepare_curl($this->url . '/api/v2/corrections/');
         $headers = array(
             "Content-Length: " . strlen($jsonstring),
             "Content-Type: application/json; charset=utf-8",
@@ -534,7 +432,7 @@ class orangedata_client {
         if (strlen($id) > 32 OR strlen($id) == 0) {
             throw new Exception('Invalid order identifier');
         }
-        $curl = is_numeric($this->api_url) ? $this->prepare_curl($this->edit_url($this->api_url,FALSE) . $this->inn . '/status/' . $id) : $this->prepare_curl($this->api_url . $this->inn . '/status/' . $id);
+        $curl = $this->prepare_curl($this->url . '/api/v2/corrections/' . $this->inn . '/status/' . $id);
         curl_setopt($curl, CURLOPT_POST, false);
         $answer = curl_exec($curl);
         $info = curl_getinfo($curl);
